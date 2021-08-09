763d193fc7b329e5a141823ae9808dd88492164c
Arman170616 <armanice9@gmail.com>
2021-08-09 21:44:39 +0600

    jobSearch_by_Company_name

--- real_website/main.py
+++ real_website/main.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get(
-    'https://www.careerjet.com.bd/search/jobs?s=python&l=Bangladesh')
+    'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
-print(html_text)
+soup = BeautifulSoup(html_text, 'lxml')
+job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
+
+company_name = job.find('h3', class_='joblist-comp-name')
+print(company_name)
