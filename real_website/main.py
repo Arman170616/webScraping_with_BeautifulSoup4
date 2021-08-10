09ab19cfddac671bf906282ba86f23bb1f27c492
Arman170616 <armanice9@gmail.com>
2021-08-10 08:40:46 +0600

    Storing_the_jobs_paragraph_in_text_files

--- real_website/main.py
+++ real_website/main.py
@@ -14,7 +14,7 @@ def find_jobs():
     soup = BeautifulSoup(html_text, 'lxml')
     jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
-    for job in jobs:
+    for index, job in enumerate(jobs):
 
         published_date = job.find(
             'span', class_='sim-posted').span.text
@@ -26,10 +26,11 @@ def find_jobs():
                 'span', class_='srp-skills').text.replace(' ', '')
             more_info = job.header.h2.a['href']
             if unfamiliar_skill not in skills:
-
-                print(f"Company Name = {company_name.strip()}")
-                print(f"Required Skills: {skills.strip()}")
-                print(f"More Info: {more_info}")
+                with open(f'real_website/posts/{index}.txt', 'w') as f:
+                    f.write(f"Company Name = {company_name.strip()}\n")
+                    f.write(f"Required Skills: {skills.strip()}\n")
+                    f.write(f"More Info: {more_info}")
+                print(f'File saved: {index}')
 
 
 if __name__ == '__main__':
